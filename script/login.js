0ac086eae4e5af5e9d116aedbdaaf42a0722f3ee
Marega <108942345+Mareguinha@users.noreply.github.com>
2024-06-10 08:58:37 -0300

    Add files via upload

---
+++ script/login.js
@@ -0,0 +1,31 @@
+const btnlogin = document.querySelector("#btnLogin");
+const inputUsuario = document.querySelector("#usuario");
+const inputSenha = document.querySelector("#senha");
+let usuarioLogado = null;
+
+(()=>{
+    usuarioLogado = localStorage.getItem("usuario");
+    if(usuarioLogado){
+        window.location.href = "/logado.html";
+    }
+
+})();
+
+btnlogin.onclick = (e) =>{
+
+    e.preventDefault();
+
+    let usuario = inputUsuario.value;
+    let senha = inputSenha.value;
+    if(usuario){
+        if(usuario == "Marega"){
+            if(senha === "123"){
+                localStorage.setItem("usuario",usuario);
+                window.location.href = "/logado.html";
+            }
+        }else{
+            inputUsuario.focus();
+        }
+    }
+
+}
